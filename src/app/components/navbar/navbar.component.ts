36f3b6a7fb3862f8e2c671a0f7a8319a0498ecea
Cristhyan <cristhyancalderon@hotmail.co.uk>
2025-08-08 11:29:29 +0100

    added themes, altered navbar and sidebar. added dummy data to accounts page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import { Router } from '@angular/router';
+import { Router} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { SignalService } from '../../services/signal.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   constructor(private router: Router) {}
 
-  navigateToHome() {
-    this.router.navigate(['']);
+  signalService = inject(SignalService);
+
+  navigateToPage(page: string) {
+    this.router.navigate([`/${page}`]);
   }
 
-  navigateToSignUp() {
-    this.router.navigate(['/sign-up']);
+  isAccountPage(): boolean {
+    return this.router.url === '/account';
   }
 
-  navigateToLogin() {
-    this.router.navigate(['/login']);
+  toggleSideMenu() {
+    this.signalService.collapsedSideMenu.set(!this.signalService.collapsedSideMenu())
   }
 }
