169f8e5b089dc95423ffc420622d4cefb2a75c47
Israel <93683580+blueishegg@users.noreply.github.com>
2025-08-19 19:30:04 +0100

    fixing deployed issue

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,15 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
-// export const serverRoutes: ServerRoute[] = [
-//   {
-//     path: '**',
-//     renderMode: RenderMode.Prerender
-//   }
-// ];
-
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   }
 ];
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Server
+//   }
+// ];
