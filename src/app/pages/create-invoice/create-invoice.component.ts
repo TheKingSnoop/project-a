8d901c9290e9bb9498e895f451bf24ac520604ac
Cristhyan <cristhyancalderon@hotmail.co.uk>
2025-08-20 12:38:39 +0100

    worked on the homepage css and some in account page

--- src/app/pages/create-invoice/create-invoice.component.ts
+++ src/app/pages/create-invoice/create-invoice.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Model } from 'survey-core';
+import { SurveyModule } from 'survey-angular-ui';
+import { themeJson } from '../../styles/themes/surveyjsTheme';
+import { json } from '../../services/surveyjs/createSurvey/json';
 
 @Component({
   selector: 'app-create-invoice',
-  imports: [],
+  imports: [SurveyModule],
   templateUrl: './create-invoice.component.html',
-  styleUrl: './create-invoice.component.scss'
+  styleUrl: './create-invoice.component.scss',
 })
-export class CreateInvoiceComponent {
+export class CreateInvoiceComponent implements OnInit {
+  model: Model;
 
+  constructor() {
+    this.model = new Model(json);
+    this.model.css = themeJson;
+  }
+
+  ngOnInit(): void {
+    this.model.onComplete.add((sender, options) => {
+      console.log(JSON.stringify(sender.data, null, 3));
+    });
+  }
 }
