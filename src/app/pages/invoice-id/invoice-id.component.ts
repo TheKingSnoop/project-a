6318ec8301b713aa709bd9e209cd833d369a423d
Cristhyan <cristhyancalderon@hotmail.co.uk>
2025-09-19 14:00:03 +0100

    invoice page updated

--- src/app/pages/invoice-id/invoice-id.component.ts
+++ src/app/pages/invoice-id/invoice-id.component.ts
@@ -1,12 +1,11 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
-import { SignalService } from '../../services/signal.service';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatTableModule } from '@angular/material/table';
 import {
@@ -15,13 +14,7 @@ import {
   BreakpointState,
 } from '@angular/cdk/layout';
 import { InvoicesService } from '../../services/invoices.service';
-
-export interface InvoiceDataTypes {
-  description: string;
-  quantity: number;
-  unitPrice: number;
-  amount: number;
-}
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-invoice-id',
@@ -34,85 +27,63 @@ export interface InvoiceDataTypes {
     CommonModule,
     MatGridListModule,
     MatTableModule,
+    MatSnackBarModule
   ],
   templateUrl: './invoice-id.component.html',
   styleUrl: './invoice-id.component.scss',
 })
 export class InvoiceIdComponent implements OnInit {
-  breakpointObserver = inject(BreakpointObserver);
-
-  //Signal to hold the invoice :id
-  invoiceId = signal<string | null>(null);
-
-  constructor(
-    private route: ActivatedRoute,
-    private invoicesService: InvoicesService
-  ) {
-    this.breakpointObserver
-      .observe([Breakpoints.Handset])
-      .subscribe((result: BreakpointState) => {
-        if (result.matches) {
-          this.isMobile.set(true);
-        } else {
-          this.isMobile.set(false);
-        }
-      });
-  }
-
-  invoice = signal<any>([]);
-
-  decodedJwtObject: any = {};
-  ngOnInit(): void {
-    // Get the ID from the route parameters
-    this.route.params.subscribe((params) => {
-      const userId = params['userId'];
-      const invoiceId = params['invoiceId'];
-      this.invoiceId.set(userId);
-
-      this.invoicesService
-        .getInvoiceById(userId, invoiceId)
-        .subscribe((result: any) => {
-          console.log(result.payload[0])
-          this.invoice.set(result.payload[0]);
-        });
-    });
-  }
-
-  invoiceData: InvoiceDataTypes[] = [
-    {
-      description:
-        'Website Development Website Development Website Development Website Development Website Development',
-      quantity: 1,
-      unitPrice: 800,
-      amount: 800,
-    },
-    {
-      description: 'Logo Design',
-      quantity: 2,
-      unitPrice: 50,
-      amount: 100,
-    },
-    {
-      description: 'Hosting Setup',
-      quantity: 1,
-      unitPrice: 100,
-      amount: 100,
-    },
-  ];
-
-  //breakpoint observer
+  // Signals
+  invoice = signal<any>(null);
   isMobile = signal<boolean>(false);
 
-  //table
+  // Table columns
   displayedColumns: string[] = [
     'description',
     'quantity',
     'unitPrice',
     'amount',
   ];
-  dataSource = this.invoiceData;
 
-  //sidebar
-  sideNavMode = computed(() => (this.isMobile() ? 'over' : 'side'));
+  // Computed properties
   columnSpan = computed(() => (this.isMobile() ? 2 : 1));
+
+  constructor(
+    private route: ActivatedRoute,
+    private invoicesService: InvoicesService,
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
+    // Observe breakpoints for mobile responsiveness
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result: BreakpointState) => {
+        this.isMobile.set(result.matches);
+      });
+  }
+
+  deleteInvoice() {
+    const userId = this.route.snapshot.params['userId'];
+    const invoiceId = this.route.snapshot.params['invoiceId'];
+    this.invoicesService.deleteInvoiceById(userId, invoiceId).subscribe(() => {
+      this.snackBar.open('Invoice deleted successfully.', 'Close', {
+      duration: 3000,
+      verticalPosition: 'bottom'
+    });
+    this.router.navigate(['/account']);
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const userId = params['userId'];
+      const invoiceId = params['invoiceId'];
+      this.invoicesService
+        .getInvoiceById(userId, invoiceId)
+        .subscribe((result: any) => {
+          this.invoice.set(result.payload[0]);
+        });
+    });
+  }
 }
